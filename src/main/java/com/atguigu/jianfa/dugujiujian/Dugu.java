6f485f6546460e4fa8b0f69dd4d8236377b454b4
linghuchong <linghuchong777888@126.com>
2019-03-26 15:33:26 +0800

    令狐冲提交

--- src/main/java/com/atguigu/jianfa/dugujiujian/Dugu.java
+++ src/main/java/com/atguigu/jianfa/dugujiujian/Dugu.java
@@ -4,5 +4,9 @@ public class Dugu {
 
     public static void main(String[] args) {
         System.out.println("jianfa  dugujiujian1!!!1");
+        System.out.println("jianfa  dugujiujian1222222");
+        System.out.println("jianfa  dugujiujian1!222");
+        System.out.println("22");
+        System.out.println("jianfa  dugujiujian1!22222!!1");
     }
 }
