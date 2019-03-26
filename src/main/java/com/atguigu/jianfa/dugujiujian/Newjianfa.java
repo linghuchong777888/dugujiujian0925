4e8ec38db34169a3da6292b1b890627b558f378f
linghuchong <linghuchong777888@126.com>
2019-03-26 16:02:43 +0800

    xinjianfa

--- src/main/java/com/atguigu/jianfa/dugujiujian/Newjianfa.java
+++ src/main/java/com/atguigu/jianfa/dugujiujian/Newjianfa.java
@@ -3,5 +3,10 @@ package com.atguigu.jianfa.dugujiujian;
 public class Newjianfa {
     public static void main(String[] args) {
         System.out.println("新剑法");
+        System.out.println("新剑法");
+        System.out.println("新剑法");
+        System.out.println("新剑法");
+        System.out.println("新剑法");
+        System.out.println("新剑法");
     }
 }
